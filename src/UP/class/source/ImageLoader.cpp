2590af98eb84052362f2b2e858deba2d35d9845b
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-11-16 17:23:37 +0100

    menu start

---
+++ src/UP/class/source/ImageLoader.cpp
@@ -0,0 +1,23 @@
+#include "ImageLoader.hpp"
+
+namespace UP {
+  ImageLoader::ImageLoader() {
+    
+  }
+  ImageLoader::ImageLoader(const FilePath& appPath,
+                  unsigned int& width,
+                  unsigned int& height) {
+  /*
+    FilePath applicationPath(argv[0]);
+  std::unique_ptr<Image> imageTriforce = loadImage(applicationPath.dirPath() + "../../GLImac-Template/assets/textures/triforce.png");
+  if ( imageTriforce == NULL ) std::cout << "Image Not Loaded" << std::endl;
+  
+  */
+  
+  }
+  
+  ImageLoader::~ImageLoader() {
+    
+  }
+  
+}
